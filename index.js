736c66d4911f16fc2fa4ea2494eb6e1dead96554
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2023-12-17 20:50:48 +0000

    added write file details

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+
 // array of questions for user
 const questions = [
-
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data);
 }
 
 // function to initialize program
 function init() {
-
+    const fileName = "./README.md";
+    
 }
 
 // function call to initialize program
