d18ab47f437791a5dc9050d68939434b1cf57b46
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2023-12-17 22:32:15 +0000

    added questions data structure

--- index.js
+++ index.js
@@ -6,6 +6,52 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'The Title Of Your Project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please Give A Description Of The Project',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Project Installation Requirements?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Project Usage',
+    },
+    {
+        type: 'list',
+        choices: ['MIT', 'GNU' ],
+        name: 'license',
+        message: 'Project License',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Project Contribution Guidelines',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Project Tests',
+    },
+    {
+        type: 'input',
+        name: 'ghUserName',
+        message: 'GitHub UserName',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter Your Email Address',
+    },
 ];
 
 // function to write README file
@@ -16,7 +62,11 @@ function writeToFile(fileName, data) {
 // function to initialize program
 function init() {
     const fileName = "./README.md";
-    
+
+    inquirer.prompt(questions).then((answers) => {
+        console.log('done');
+        console.log(JSON.stringify(answers, null, '  '));
+      });
 }
 
 // function call to initialize program
